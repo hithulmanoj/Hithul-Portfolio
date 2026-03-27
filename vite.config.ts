9247334a781f54b38426afec2a73c952ed4360e0
Hithul Manoj <iamhithulmanoj@gmail.com>
2026-03-27 11:33:30 +0530

    Add deployment script

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hithul-visions-unfold-main/",
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
