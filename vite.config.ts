9d9f23b325d1c076f223154c79f288f7e891d1f2
Hithul Manoj <iamhithulmanoj@gmail.com>
2026-03-27 11:56:26 +0530

    Update base path to

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/hithul-visions-unfold-main/",
+  base: "/Hithul-Portfolio/",
   server: {
     host: "::",
     port: 8080,
